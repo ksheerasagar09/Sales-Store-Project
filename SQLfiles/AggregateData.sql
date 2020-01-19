cc6e76665469c1d0a1082d506c3d77a477c7b6ae
ksheerasagar09 <47727203+ksheerasagar09@users.noreply.github.com>
2020-01-19 18:02:11 +0530

    Update AggregateData.sql

--- SQLfiles/AggregateData.sql
+++ SQLfiles/AggregateData.sql
@@ -1,16 +1,17 @@
-select date, store_location, round(sum(SP) - sum(CP)) as location_profit 
-from store_data_daily
+--Aggregate Data at the Store Location Level
+Select date, store_location, round(sum(SP) - sum(CP)) as location_profit 
+ from store_data_daily
 group by date, store_location
 order by location_profit desc
-into outfile "/store_files_mysql/loc_wise_profit_agg.csv"
+Into outfile "/store_files_mysql/loc_wise_profit_agg.csv"
 fields terminated by ","
 lines terminated by "\n";
 
-
+--Aggregate Data at the Store Level
 select date, store_id, round(sum(SP) - sum(CP)) as store_profit 
 from store_data_daily
 group by date, store_id
 order by store_profit desc
-into outfile "/store_files_mysql/store_wise_profit_agg.csv"
+Into outfile "/store_files_mysql/store_wise_profit_agg.csv"
 fields terminated by ","
 lines terminated by "\n";
